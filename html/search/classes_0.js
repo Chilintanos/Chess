730ca78b9c18abd4e3e9774ea2a49851400d972c
Chilintanos <148976485+Chilintanos@users.noreply.github.com>
2023-10-25 15:52:03 +0300

    Add files via upload

---
+++ html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['color_5frect_5fchanger_0',['color_rect_changer',['../classcolor__rect__changer.html',1,'']]]
+];
---
+++ html/search/functions_3.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['is_5fcheck_0',['is_check',['../struct_field.html#a2fb9df0af460ecdcca8034edf163e50c',1,'Field']]],
+  ['is_5fin_5ffield_1',['is_in_field',['../struct_field.html#a429c9c805b22a027e6dc2dbc68c37cea',1,'Field']]],
+  ['is_5fking_5fnearby_2',['is_king_nearby',['../struct_field.html#a5271b72e7126304c002df4c8e3c830be',1,'Field']]],
+  ['is_5fmate_3',['is_mate',['../struct_field.html#a7f7cf1dd154cb93c7cbffbb5f02e8bad',1,'Field']]],
+  ['is_5fmouse_5fover_4',['is_mouse_over',['../class_figure.html#a24920ffd56bd670229f22b8e88b1a837',1,'Figure']]],
+  ['is_5fwindow_5fopen_5',['is_window_open',['../class_game_manager.html#a639cfe04af09c17793cdb29c43bdabc1',1,'GameManager']]]
+];
