d3cea27e0edcf96d9b81a3627e924e97d6489671
Chilintanos <148976485+Chilintanos@users.noreply.github.com>
2023-11-07 17:08:23 +0300

    Delete doc directory

--- doc/html/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['field_0',['Field',['../struct_field.html',1,'']]],
-  ['figure_1',['Figure',['../class_figure.html',1,'']]],
-  ['figures_2',['figures',['../struct_field.html#a3fa9ced13701bf7181e25784e29d22cf',1,'Field']]]
-];
--- doc/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['check_5fcheck_0',['check_check',['../struct_field.html#ad6bdb39fce3c508561c8983ba53ddbe4',1,'Field']]]
-];
